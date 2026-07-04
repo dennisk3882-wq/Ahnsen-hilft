8fb7641de7a1b4d9fec94a68cce6cdef1707632b
dennisk3882-wq <dennisk3882@gmail.com>
2026-07-04 12:33:02 +0200

    Update veranstaltungen_models.py

--- veranstaltungen_models.py
+++ veranstaltungen_models.py
@@ -1,1 +1,22 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
 
+from database import Base
+
+
+class Veranstaltung(Base):
+    __tablename__ = "veranstaltungen"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    titel = Column(String, nullable=False)
+    datum = Column(String)
+    uhrzeit = Column(String)
+    ort = Column(String)
+    beschreibung = Column(Text)
+    ansprechpartner = Column(String)
+
+    bild_base64 = Column(Text, nullable=True)
+
+    aktiv = Column(String, default="Ja")
+    erstellt_am = Column(DateTime, default=datetime.utcnow)
