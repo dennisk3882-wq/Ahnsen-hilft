4a31c4007a638af02e87882203d7614e2b18b36e
dennisk3882-wq <dennisk3882@gmail.com>
2026-07-04 23:23:45 +0200

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,12 +1,10 @@
-
-# whatsapp.py
-
+import base64
 import requests
+
 from config import WHATSAPP_TOKEN, PHONE_NUMBER_ID
 
 
 def send_whatsapp_message(to, text):
-    """Sendet eine Textnachricht über WhatsApp."""
     url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
 
     headers = {
@@ -26,5 +24,35 @@ def send_whatsapp_message(to, text):
     response = requests.post(url, headers=headers, json=data, timeout=20)
 
     print("WhatsApp Antwort:", response.status_code, response.text)
+
+    return response
+
+
+def send_whatsapp_image(to, bild_base64, caption=""):
+    url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "image",
+        "image": {
+            "caption": caption,
+            "data": bild_base64
+        }
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=data,
+        timeout=30,
+    )
+
+    print("WhatsApp Bild:", response.status_code, response.text)
 
     return response
