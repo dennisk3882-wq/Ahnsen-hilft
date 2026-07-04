cee0cf20116cdfa7b874d4567df1cf17831d35ce
dennisk3882-wq <dennisk3882@gmail.com>
2026-07-04 12:34:08 +0200

    Update veranstaltungen_crud.py

--- veranstaltungen_crud.py
+++ veranstaltungen_crud.py
@@ -1,1 +1,101 @@
+import base64
 
+from database import Base, engine, SessionLocal
+from veranstaltungen_models import Veranstaltung
+
+
+def init_veranstaltungen_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def save_veranstaltung(titel, datum, uhrzeit, ort, beschreibung, ansprechpartner, bild_bytes=None):
+    db = SessionLocal()
+
+    try:
+        bild_base64 = None
+
+        if bild_bytes:
+            bild_base64 = base64.b64encode(bild_bytes).decode("utf-8")
+
+        veranstaltung = Veranstaltung(
+            titel=titel,
+            datum=datum,
+            uhrzeit=uhrzeit,
+            ort=ort,
+            beschreibung=beschreibung,
+            ansprechpartner=ansprechpartner,
+            bild_base64=bild_base64,
+            aktiv="Ja",
+        )
+
+        db.add(veranstaltung)
+        db.commit()
+        db.refresh(veranstaltung)
+
+        return veranstaltung
+
+    finally:
+        db.close()
+
+
+def get_aktive_veranstaltungen():
+    db = SessionLocal()
+
+    try:
+        return (
+            db.query(Veranstaltung)
+            .filter(Veranstaltung.aktiv == "Ja")
+            .order_by(Veranstaltung.datum.asc())
+            .all()
+        )
+
+    finally:
+        db.close()
+
+
+def get_alle_veranstaltungen():
+    db = SessionLocal()
+
+    try:
+        return (
+            db.query(Veranstaltung)
+            .order_by(Veranstaltung.datum.asc())
+            .all()
+        )
+
+    finally:
+        db.close()
+
+
+def get_veranstaltung(veranstaltung_id):
+    db = SessionLocal()
+
+    try:
+        return (
+            db.query(Veranstaltung)
+            .filter(Veranstaltung.id == veranstaltung_id)
+            .first()
+        )
+
+    finally:
+        db.close()
+
+
+def delete_veranstaltung(veranstaltung_id):
+    db = SessionLocal()
+
+    try:
+        veranstaltung = (
+            db.query(Veranstaltung)
+            .filter(Veranstaltung.id == veranstaltung_id)
+            .first()
+        )
+
+        if veranstaltung:
+            db.delete(veranstaltung)
+            db.commit()
+
+        return veranstaltung
+
+    finally:
+        db.close()
