91e1cdad1ee2e748bd275d60d0dec46f8bfdd72f
dennisk3882-wq <dennisk3882@gmail.com>
2026-07-30 15:29:22 +0200

    Include account schema in platform bootstrap

--- quiz-app/platform-storage.js
+++ quiz-app/platform-storage.js
@@ -1,7 +1,8 @@
 'use strict';
 const db=require('./platform-db');
+const account=require('./account-storage');
 const social=require('./platform-social-storage');
 const game=require('./platform-game-storage');
 const ops=require('./platform-ops-storage');
-async function ensureReady(){if(!db.enabled())return false;await Promise.all([social.ensureReady(),game.ensureReady(),ops.ensureReady()]);return true;}
-module.exports={enabled:db.enabled,ensureReady,safeText:db.safeText,safeCode:db.safeCode,...social,...game,...ops,_test:{normalizeQuestions:db.normalizeQuestions,pairIds:db.pairIds,randomCode:db.randomCode}};
+async function ensureReady(){if(!db.enabled())return false;await Promise.all([account.ensureReady(),social.ensureReady(),game.ensureReady(),ops.ensureReady()]);return true;}
+module.exports={enabled:db.enabled,ensureReady,safeText:db.safeText,safeCode:db.safeCode,...social,...game,...ops,account,_test:{normalizeQuestions:db.normalizeQuestions,pairIds:db.pairIds,randomCode:db.randomCode}};
