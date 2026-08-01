aabd91b9077610d8edc1bb4182acae50f15f7bbd
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-01 15:07:55 +0200

    QuizTime erst nach geordneter Schemaprüfung starten

--- quiz-app/start.js
+++ quiz-app/start.js
@@ -3,5 +3,12 @@
 process.env.QUIZ_TITLE = 'QuizTime';
 require('./test-pg-ssl-compat');
 require('./answer-layout-patch');
-require('./platform-bootstrap');
-require('./server');
+
+(async () => {
+  await require('./startup-schema').prepareStartupSchema();
+  require('./platform-bootstrap');
+  require('./server');
+})().catch(error => {
+  console.error('QuizTime konnte nicht sicher gestartet werden:', error);
+  process.exitCode = 1;
+});
