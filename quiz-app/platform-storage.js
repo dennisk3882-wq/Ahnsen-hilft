56d1e7ba103aa8864ff7a33cd4277aa4415cf0fe
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-05 16:46:19 +0200

    QuizTime 13.1: Plattform-Schema sequenziell starten

--- quiz-app/platform-storage.js
+++ quiz-app/platform-storage.js
@@ -4,5 +4,12 @@ const account=require('./account-storage');
 const social=require('./platform-social-storage');
 const game=require('./platform-game-storage');
 const ops=require('./platform-ops-storage');
-async function ensureReady(){if(!db.enabled())return false;await Promise.all([account.ensureReady(),social.ensureReady(),game.ensureReady(),ops.ensureReady()]);return true;}
+async function ensureReady(){
+  if(!db.enabled())return false;
+  await account.ensureReady();
+  await social.ensureReady();
+  await game.ensureReady();
+  await ops.ensureReady();
+  return true;
+}
 module.exports={enabled:db.enabled,ensureReady,safeText:db.safeText,safeCode:db.safeCode,...social,...game,...ops,account,_test:{normalizeQuestions:db.normalizeQuestions,pairIds:db.pairIds,randomCode:db.randomCode}};
