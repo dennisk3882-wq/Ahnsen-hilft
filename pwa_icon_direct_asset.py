6e8b2605c7d2a9e44b641b067c5dbb7a8bfb9159
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-13 17:23:03 +0200

    Direkte PWA-Iconquelle ohne Base64 ergänzen

---
+++ pwa_icon_direct_asset.py
@@ -0,0 +1,6 @@
+"""Direct WebP source for the Ahnsen PWA launcher icon."""
+from pwa_icon_assets.icon_v7_bytes_00 import DATA as _PART_00
+from pwa_icon_assets.icon_v7_bytes_01 import DATA as _PART_01
+from pwa_icon_assets.icon_v7_bytes_02 import DATA as _PART_02
+
+STONE_ICON_BYTES = _PART_00 + _PART_01 + _PART_02
