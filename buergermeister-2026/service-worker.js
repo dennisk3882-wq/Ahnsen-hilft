9cfcc55f1fa9f20cbb10f03925c88866663f7461
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-16 12:06:02 +0200

    Refresh PWA cache for strategic v4

--- buergermeister-2026/service-worker.js
+++ buergermeister-2026/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='buergermeister-2026-v3';
+const CACHE='buergermeister-2026-v4';
 const ASSETS=['./','index.html','styles.css','game.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res}).catch(()=>caches.match('./'))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
