cf10a9bd9d9e1fe415181caa9f0e2efe4b2b6812
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-17 13:06:21 +0200

    Cache modern Bürgermeister theme

--- BuergermeisterPWA/sw.js
+++ BuergermeisterPWA/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'buergermeister-1992-plus-v15';
-const ASSETS = ["./","index.html","styles.css","game-engine.js","app.js","manifest.webmanifest","icons/icon.svg","icons/icon-192.png","icons/icon-512.png","assets/stage-kuhdorf.svg","assets/stage-dorf.svg","assets/stage-grosses-dorf.svg","assets/stage-kleinstadt.svg","assets/stage-stadt.svg","assets/stage-grossstadt.svg","assets/stage-moderne-stadt.svg","assets/stage-metropole.svg"];
+const CACHE = 'buergermeister-1992-plus-v16';
+const ASSETS = ["./","index.html","styles.css","modern-theme.css","game-engine.js","app.js","manifest.webmanifest","icons/icon.svg","icons/icon-192.png","icons/icon-512.png","assets/stage-kuhdorf.svg","assets/stage-dorf.svg","assets/stage-grosses-dorf.svg","assets/stage-kleinstadt.svg","assets/stage-stadt.svg","assets/stage-grossstadt.svg","assets/stage-moderne-stadt.svg","assets/stage-metropole.svg"];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
