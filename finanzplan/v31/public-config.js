5125b0f4cda9f07963885a96b08ec5f27fcc557c
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-29 18:26:32 +0200

    Preserve public cloud defaults across new household setup

--- finanzplan/v31/public-config.js
+++ finanzplan/v31/public-config.js
@@ -3,14 +3,24 @@
   const PUBLIC_SUPABASE_URL='https://yhsuuoexxjejboqbrvuk.supabase.co';
   const PUBLIC_SUPABASE_KEY='sb_publishable_e4LMvDTTT41I3fAKZjjjJw_zVTLcTRy';
   const PUBLIC_BACKEND_URL='https://yhsuuoexxjejboqbrvuk.supabase.co/functions/v1/finanzplan-api';
-  data.integrations=data.integrations||{};
-  data.integrations.cloud=data.integrations.cloud||{};
-  data.integrations.n26=data.integrations.n26||{};
-  data.integrations.ai=data.integrations.ai||{};
-  if(!data.integrations.cloud.url)data.integrations.cloud.url=PUBLIC_SUPABASE_URL;
-  if(!data.integrations.cloud.publishableKey)data.integrations.cloud.publishableKey=PUBLIC_SUPABASE_KEY;
-  if(!data.integrations.backendUrl)data.integrations.backendUrl=PUBLIC_BACKEND_URL;
-  if(!data.integrations.n26.backendUrl)data.integrations.n26.backendUrl=PUBLIC_BACKEND_URL;
-  if(!data.integrations.ai.backendUrl)data.integrations.ai.backendUrl=PUBLIC_BACKEND_URL;
-  window.FinanzPublicConfig=Object.freeze({supabaseUrl:PUBLIC_SUPABASE_URL,supabasePublishableKey:PUBLIC_SUPABASE_KEY,backendUrl:PUBLIC_BACKEND_URL});
+  function applyPublicConfig(target=data){
+    target.integrations=target.integrations||{};
+    target.integrations.cloud=target.integrations.cloud||{};
+    target.integrations.n26=target.integrations.n26||{};
+    target.integrations.ai=target.integrations.ai||{};
+    if(!target.integrations.cloud.url)target.integrations.cloud.url=PUBLIC_SUPABASE_URL;
+    if(!target.integrations.cloud.publishableKey)target.integrations.cloud.publishableKey=PUBLIC_SUPABASE_KEY;
+    if(!target.integrations.backendUrl)target.integrations.backendUrl=PUBLIC_BACKEND_URL;
+    if(!target.integrations.n26.backendUrl)target.integrations.n26.backendUrl=target.integrations.backendUrl||PUBLIC_BACKEND_URL;
+    if(!target.integrations.ai.backendUrl)target.integrations.ai.backendUrl=target.integrations.backendUrl||PUBLIC_BACKEND_URL;
+    return target;
+  }
+  applyPublicConfig();
+  const baseSave=saveData;
+  saveData=function(reason='Änderung gespeichert'){applyPublicConfig(data);return baseSave(reason)};
+  if(typeof emptyProjectData==='function'){
+    const baseEmpty=emptyProjectData;
+    emptyProjectData=function(preservePreferences=true){return applyPublicConfig(baseEmpty(preservePreferences))};
+  }
+  window.FinanzPublicConfig=Object.freeze({supabaseUrl:PUBLIC_SUPABASE_URL,supabasePublishableKey:PUBLIC_SUPABASE_KEY,backendUrl:PUBLIC_BACKEND_URL,apply:applyPublicConfig});
 })();
