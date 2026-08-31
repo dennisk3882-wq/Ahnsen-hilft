1d2297885a82ede81eabdb8436d439980fa48fce
dennisk3882-wq <dennisk3882@gmail.com>
2026-08-31 14:03:11 +0200

    Handle Sparkasse callback and autosync

--- finanzplan/v31/runtime.js
+++ finanzplan/v31/runtime.js
@@ -7,10 +7,11 @@
   async function startupOnline(){if(!navigator.onLine)return;
     const cloud=data.integrations?.cloud;if(cloud?.enabled&&cloud.url&&cloud.publishableKey){const s=await quiet('cloud session',()=>FinanzCloud.getSession());if(s?.access_token&&cloud.householdId&&ageMinutes(cloud.lastSync)>5)await quiet('cloud sync',()=>FinanzCloud.fullSync())}
     const n=data.integrations?.n26;if(n?.sessionReady&&n.localAccountId&&n.backendUrl&&ageMinutes(n.lastSync)>15)await quiet('N26 auto sync',()=>FinanzN26.sync({days:180,reconcile:true}));
+    const sp=data.integrations?.sparkasse;if(globalThis.FinanzSparkasse&&sp?.sessionReady&&sp.localAccountId&&ageMinutes(sp.lastSync)>15)await quiet('Sparkasse auto sync',()=>FinanzSparkasse.sync({days:180,reconcile:true}));
     if(data.integrations?.push?.enabled&&data.integrations?.cloud?.householdId)await quiet('push schedule',()=>FinanzPush.syncJobs());
   }
   window.finanzplanV3Start=async function(options={}){await baseStart(options);FinanzPremium.ensurePremiumData();data.version=runtimeVersion();data.schemaVersion=3;let changed=FinanzPremium.maybeCreatePreviousMonthReport();if(changed)saveData('Automatischer Monatsbericht erstellt');
-    if(new URL(location.href).searchParams.get('code'))await quiet('N26 callback',()=>FinanzN26.handleCallback());
+    if(new URL(location.href).searchParams.get('code')){let handled=false;if(globalThis.FinanzSparkasse)handled=!!(await quiet('Sparkasse callback',()=>FinanzSparkasse.handleCallback()));if(!handled)await quiet('N26 callback',()=>FinanzN26.handleCallback())}
     await startupOnline();data.version=runtimeVersion();if(!data.settings?.vault)await window.__finanzplanStorage.saveState(data).catch(()=>{});else if(window.__vaultKey)await persistVault().catch(()=>{});renderAll();
   };
   window.addEventListener('online',()=>startupOnline().then(()=>renderAll()),{passive:true});
