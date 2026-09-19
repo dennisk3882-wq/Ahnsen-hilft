458d43448c137a434f74e5480a1ceacdf3f26569
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-19 13:29:44 +0200

    Syndikat v5.1: include visual story module in runtime build

--- Syndikat/tools/build-core.cjs
+++ Syndikat/tools/build-core.cjs
@@ -9,6 +9,7 @@ const modules=[
   "src/modules/30-meta-progression.js",
   "src/modules/40-ai-espionage-events.js",
   "src/modules/50-economy-property.js",
+  "src/modules/55-visual-story.js",
   "src/modules/60-cloud-online.js",
   "src/modules/70-decisions.js",
   "src/modules/80-justice.js",
