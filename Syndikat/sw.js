036b011cbc4175e1ff29aaa4cbec66b982c0ba3f
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-19 11:46:42 +0200

    Syndikat v4.5: cache cloud adapter and support in-app updates

--- Syndikat/sw.js
+++ Syndikat/sw.js
@@ -1,5 +1,5 @@
-const CACHE='syndikat-v4-3';
-const CORE=['./','./index.html','./style.css','./js/core.js','./assets/start-noir.svg','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
+const CACHE='syndikat-v4-5';
+const CORE=['./','./index.html','./style.css','./js/core.js','./cloud-config.js','./js/cloud.js','./assets/start-noir.svg','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('syndikat-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
@@ -12,3 +12,4 @@ self.addEventListener('fetch',e=>{
   }
   e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));}return r;})));
 });
+self.addEventListener('message',e=>{if(e.data?.type==='SKIP_WAITING')self.skipWaiting();});
